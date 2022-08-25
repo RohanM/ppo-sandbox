ea5e5674d42351d3a113ad1ab32490bef5cf487b
Rohan Mitchell <rohan@rohanmitchell.com>
2022-08-25 14:26:18 +1000

    Initial import - part-made PPO from scratch implementation

---
+++ README.md
@@ -0,0 +1,5 @@
+# PPO Sandbox & Experiments
+
+- https://towardsdatascience.com/proximal-policy-optimization-tutorial-part-1-actor-critic-method-d53f9afffbf6
+- https://towardsdatascience.com/proximal-policy-optimization-tutorial-part-2-2-gae-and-ppo-loss-fe1b3c5549e8
+
---
+++ ppo-from-scratch.py
@@ -0,0 +1,70 @@
+import gym
+import numpy as np
+from torch import nn, tensor, Tensor
+
+# Observations: Tensor[8]
+# Actions: Four discrete actions
+class ActorModel(nn.Module):
+    def __init__(self, num_input=8, num_hidden=32, num_output=4):
+        super().__init__()
+        self.linear1 = nn.Linear(num_input, num_hidden)
+        self.linear2 = nn.Linear(num_hidden, num_hidden)
+        self.linear3 = nn.Linear(num_hidden, num_output)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=0)
+
+    def forward(self, x: Tensor):
+        out = self.linear1(x)
+        out = self.relu(out)
+        out = self.linear2(out)
+        out = self.relu(out)
+        out = self.linear3(out)
+        out = self.softmax(out)
+        return out
+
+
+class CriticModel(nn.Module):
+    def __init__(self, num_input=8, num_hidden=32):
+        super().__init__()
+        num_output = 1
+
+        self.linear1 = nn.Linear(num_input, num_hidden)
+        self.linear2 = nn.Linear(num_hidden, num_hidden)
+        self.linear3 = nn.Linear(num_hidden, num_output)
+        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
+
+    def forward(self, x: Tensor):
+        out = self.linear1(x)
+        out = self.relu(out)
+        out = self.linear2(out)
+        out = self.relu(out)
+        out = self.linear3(out)
+        return self.tanh(out)
+
+
+env = gym.make('LunarLander-v2')
+n_state = env.observation_space.shape[0]
+n_actions = env.action_space.n
+
+ppo_steps = 128
+actor = ActorModel(num_input=n_state, num_output=n_actions)
+critic = CriticModel()
+
+state = env.reset()
+
+
+for i in range(ppo_steps):
+    state_input = tensor(state)
+    action_dist = actor(state_input)
+    q_value = critic(state_input)
+    action = np.random.choice(n_actions, p=action_dist.detach().numpy())
+
+    observation, reward, done, info = env.step(action)
+    env.render()
+
+    state = observation
+
+    if done:
+        env.reset()
+        break
