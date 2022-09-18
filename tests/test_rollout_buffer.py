80387fe7ec11a4498ee39fa08a6d011ebf423bcf
Rohan Mitchell <rohan@rohanmitchell.com>
2022-09-18 13:14:09 +1000

    Update RolloutBuf specs

--- tests/test_rollout_buffer.py
+++ tests/test_rollout_buffer.py
@@ -1,80 +1,85 @@
 import pytest
 import torch
 from torch import tensor
+import numpy as np
 from ppo_from_scratch import RolloutBuffer
 
 @pytest.fixture
 def buf():
-    buf = RolloutBuffer()
+    buf = RolloutBuffer(8)
     for i in range(3):
         buf.add_obs(
-            state=tensor([1, 2, 3]),
-            action=1,
+            state=tensor([1, 2, 3, 4, 5, 6, 7, 8]),
+            action=tensor([1]),
             action_logp=tensor(0.5),
-            mask=[True, True, False][i],
-            reward=1
+            mask=np.array([[True, True, False][i]]),
+            reward=np.array([1])
         )
     buf.add_obs(
-        state=tensor([1, 2, 3]),
-        action=1,
+        state=tensor([1, 2, 3, 4, 5, 6, 7, 8]),
+        action=tensor([1]),
         action_logp=tensor(0.5),
-        mask=True,
-        reward=1
+        mask=np.array([True]),
+        reward=np.array([1])
     )
     buf.add_obs(
-        state=tensor([1, 2, 3]),
-        action=1,
+        state=tensor([1, 2, 3, 4, 5, 6, 7, 8]),
+        action=tensor([1]),
         action_logp=tensor(0.5),
-        mask=True,
-        reward=1
+        mask=np.array([True]),
+        reward=np.array([1])
     )
+    buf.prep_data(tensor([[[0]], [[1]], [[2]], [[3]], [[4]]]))
     return buf
 
 def test_get_states(buf):
     assert torch.equal(
         buf.get_states(),
-        tensor([[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3]])
+        tensor([
+            [1, 2, 3, 4, 5, 6, 7, 8],
+            [1, 2, 3, 4, 5, 6, 7, 8],
+            [1, 2, 3, 4, 5, 6, 7, 8],
+            [1, 2, 3, 4, 5, 6, 7, 8],
+            [1, 2, 3, 4, 5, 6, 7, 8],
+        ])
     )
 
 def test_get_actions(buf):
-    assert torch.equal(buf.get_actions(), tensor([[1], [1], [1], [1], [1]]))
+    assert torch.equal(buf.get_actions(), tensor([1, 1, 1, 1, 1]))
 
 def test_get_actions_logps(buf):
-    assert torch.equal(buf.get_actions_logps(), tensor([[0.5], [0.5], [0.5], [0.5], [0.5]]))
+    assert torch.equal(buf.get_actions_logps(), tensor([0.5, 0.5, 0.5, 0.5, 0.5]))
 
 def test_get_masks(buf):
-    assert buf.get_masks() == [True, True, False, True, True]
+    assert torch.equal(buf.get_masks(), tensor([True, True, False, True, True]))
 
 def test_get_rewards(buf):
-    assert torch.equal(buf.get_rewards(), tensor([[1], [1], [1], [1], [1]]))
+    assert torch.equal(buf.get_rewards(), tensor([1, 1, 1, 1, 1]).float())
 
 def test_get_returns(buf):
-    returns = buf.get_returns()
-    expected = tensor([[3], [2], [1], [2], [1]]).float()
-    assert torch.equal(returns, expected)
+    assert torch.isclose(
+        buf.get_returns(),
+        tensor([[3], [2], [1], [6], [5]]).float(),
+        atol=0.2
+    ).all()
 
 def test_build_advantages(buf):
-    advantages = buf.build_advantages(tensor([0, 1, 2, 3, 4]), gamma=0.9, lmbda=0.8)
-    advantages = buf.get_advantages()
-    expected = tensor([[2.68], [1.08], [-1.00], [2.03], [0.60]])
-    assert torch.isclose(advantages, expected, atol=0.01).all()
+    assert torch.isclose(
+        buf.get_advantages(),
+        tensor([[3], [1], [-1], [3], [1]]).float(),
+        atol=0.2
+    ).all()
 
 def test_dataset_len(buf):
-    buf.build_returns()
-    buf.build_advantages(tensor([0, 1, 2, 3, 4]), gamma=0.9, lmbda=0.8)
     assert len(buf) == 5
 
 def test_dataset_getitem(buf):
-    buf.build_returns()
-    buf.build_advantages(tensor([0, 1, 2, 3, 4]), gamma=0.9, lmbda=0.8)
     data = buf[0]
     assert len(data) == 7
-    assert torch.equal(data[0], tensor([1, 2, 3]))
+    assert torch.equal(data[0], tensor([1, 2, 3, 4, 5, 6, 7, 8]))
     assert data[1] == 1
     assert torch.equal(data[2], tensor(0.5000))
-    assert data[3] == True
+    assert data[3].all()
     assert data[4] == 1
-    assert torch.equal(data[5], tensor([3.]))
-    assert torch.equal(data[6], tensor([2.6776]))
-
-# Check expected values for advantages against equations
+    assert torch.isclose(data[5], tensor([2.9676]), atol=0.01)
+    assert torch.isclose(data[6], tensor([2.9676]), atol=0.01)
